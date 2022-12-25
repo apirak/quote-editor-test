9027ec578cf8e60464ec3d04aff1a7f32b290c81
Apirak Panatkool <apirakb@gmail.com>
2022-12-26 06:59:11 +0700

    Add test for turbo

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -5,6 +5,13 @@ class QuotesTest < ApplicationSystemTestCase
     @quote = quotes(:first)
   end
 
+  test "Show a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
   test "Creating a new quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
@@ -17,6 +24,20 @@ class QuotesTest < ApplicationSystemTestCase
 
     assert_selector "h1",  text: "Quotes"
     assert_text "Capybara quote"
+  end
+
+  test "Update a quote" do
+    visit quotes_path
+    assert_selector "h1", test: "Quotes"
+
+    click_on "Edit", match: :first
+    fill_in "Name", with: "Update quote"
+
+    assert_selector "h1", text: "Quotes"
+    click_on "Update quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Update quote"
   end
 
   test "Destroying a quote" do
