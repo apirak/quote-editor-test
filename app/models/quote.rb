82909c27242fbe9bb505d0fd3b0109372c7cdacb
Apirak Panatkool <apirakb@gmail.com>
2022-12-29 09:51:04 +0700

    add turbo update and destroy

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,14 +3,39 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> {
-                        broadcast_prepend_to "quotes",
-                                             partial: "quotes/quote",
-                                             locals: {
-                                               quote: self,
-                                             },
-                                             target: "quotes"
-                      }
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  # after_create_commit lambda {
+  #                       broadcast_prepend_to "quotes",
+  #                                            partial: "quotes/quote",
+  #                                            locals: {
+  #                                              quote: self,
+  #                                            },
+  #                                            target: "quotes"
+  #                     }
 end
 
 # broadcast_prepend_to(*streamables, target: broadcast_target_default, **rendering)
+
+# Sends
+# <turbo-stream action="prepend" target="clearances">
+#  <template>
+#   <div id="clearance_5">My Clearance</div>
+#  </template>
+# </turbo-stream>
+# to the stream named "identity:2:clearances"
+# clearance.broadcast_prepend_to examiner.identity, :clearances, target: "clearances"
+
+# Sends
+# <turbo-stream action="prepend" target="clearances">
+#   <template>
+#     <div id="clearance_5">Other partial</div>
+#   </template>
+# </turbo-stream>
+# to the stream named "identity:2:clearances"
+# clearance.broadcast_prepend_to examiner.identity, :clearances, target: "clearances",
+#   partial: "clearances/other_partial", locals: { a: 1 }
